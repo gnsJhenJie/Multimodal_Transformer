f45da5a9489ae83912da7d5f3be0f7640c17b190
minano430 <sominano4303@gmail.com>
2022-04-26 19:49:11 +0800

    Untrack evaluation script

--- experiments/nuScene/results/result_calc.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('lane_transformer_me_8_ade_full.csv')
-data = data[~data.value.isnull()]
-print('Total ADE : ',data['value'].mean())
-
-data = pd.read_csv('lane_transformer_me_8_fde_full.csv')
-data = data[~data.value.isnull()]
-print('Total FDE : ',data['value'].mean())
